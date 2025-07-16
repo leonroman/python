f2ea22ebcdfb19643355b87cb68d392dd4ed9c28
Roman Leon <leon_roman@inbox.ru>
2025-07-16 17:43:14 +0200

    upd: day1 scripts with output inside

--- day1/hello.py
+++ day1/hello.py
@@ -1,3 +1,8 @@
 # This is a simple Python script that prints "Hello, World!" to the console.
 # To execute this script, run python hello.py from the command line.
 print ("Hello, World!")
+
+
+
+# $ python day1/hello.py 
+# Hello, World!
--- day1/indentation.py
+++ day1/indentation.py
@@ -1,11 +1,26 @@
 # This code demonstrates the use of indentation in Python.
 # Indentation is crucial in Python as it defines the blocks of code.
 # Here we have an output loop that prints "Hello" and the value of i 2 times.
+print ("First loop starts now")
 for i in range(2):
     print ("Hello")
-    print(i)
+    print("Value of i is:", i)
 
+print ("Second loop starts now")
 # Here we have another loop that prints "Hi" 2 times and only once the last value of i (the last iteration).
 for i in range(2):
     print ("Hi")
-print(i)
+print("Value of i is:", i)
+
+
+
+# $ python day1/indentation.py 
+# First loop starts now
+# Hello
+# Value of i is: 0
+# Hello
+# Value of i is: 1
+# Second loop starts now
+# Hi
+# Hi
+# Value of i is: 1

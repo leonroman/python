1b195f439400b71b96c693023e76d5541403f2a7
Roman Leon <leon_roman@inbox.ru>
2025-07-16 17:38:30 +0200

    add: day1/indentation.py

---
+++ day1/indentation.py
@@ -0,0 +1,11 @@
+# This code demonstrates the use of indentation in Python.
+# Indentation is crucial in Python as it defines the blocks of code.
+# Here we have an output loop that prints "Hello" and the value of i 2 times.
+for i in range(2):
+    print ("Hello")
+    print(i)
+
+# Here we have another loop that prints "Hi" 2 times and only once the last value of i (the last iteration).
+for i in range(2):
+    print ("Hi")
+print(i)
